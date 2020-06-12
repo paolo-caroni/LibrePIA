7947ca87317df1c6492fffc4c8b1f8e3b94ef162
paolo-caroni <kenren89@gmail.com>
2020-06-12 18:20:45 +0200

    set to 261 max name characters (stb can have long name)

--- plot_style_table.h
+++ plot_style_table.h
@@ -1,6 +1,3 @@
-
-
-
 /*
  * LibrePIA is an attempt to create a LIBrary to Read and Edit PIA plot files (.pc3, .pmp, .stb, .ctb).
  *
@@ -17,20 +14,27 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* declare ctb int, float, string, matrix, etc*/
-char file_description[1027],/*aci_table_available[6],apply_factor[5],*/aci_table[256][20];
-int aci_table_available,apply_factor,custom_lineweight_display_units,min_color=0,max_color=254;
+/* declare ctb and stb int, float, string, matrix, etc*/
+
+/* description of the file*/
+char file_description[1027];
+/* aci_table_available better using boolean? or char?*/
+int aci_table_available;
+/* scale_factor*/
 float scale_factor;
-/* boolean, define TRUE and FALSE
-_Bool aci_table_available;
-const int TRUE = 1;
-const int FALSE = 0;
-*/
+/* apply_factor better using boolean? or char?*/
+int apply_factor;
+/* custom_lineweight_display_units*/
+int custom_lineweight_display_units;
+/* aci_table (only for ctb)*/
+char aci_table[256][20];
+/* plot_style*/
 int color_number[255];
-char name[255][12];
-char localized_name[255][12];
+char name[255][261];
+char localized_name[255][261];
 char description[255][4869];
 int color[255];
+/* mode_color is optional*/
 int mode_color[255];
 int color_policy[255];
 int physical_pen_number[255];
@@ -38,11 +42,21 @@ int virtual_pen_number[255];
 int screen[255];
 float linepattern_size[255];
 int linetype[255];
-/* better using boolean? or char?*/
+/* adaptive_linetype better using boolean? or char?*/
 int adaptive_linetype[255];
 int lineweight[255];
 int fill_style[255];
 int end_style[255];
 int join_style[255];
-
+/* custom_lineweight_table*/
 float custom_lineweight_table[27];
+
+/* other useful entities*/
+int min_style=0,max_style=254,style_number;
+/* boolean, define TRUE and FALSE
+_Bool aci_table_available;
+_Bool apply_factor;
+_Bool adaptive_linetype[255];
+const int TRUE = 1;
+const int FALSE = 0;
+*/
