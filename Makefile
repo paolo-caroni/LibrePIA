0787e07a337eaf5ac59f0facc337d4b9240ee3d0
paolo-caroni <kenren89@gmail.com>
2020-06-19 19:58:39 +0200

    add other example programs

--- Makefile
+++ Makefile
@@ -5,10 +5,14 @@ LibrePIA:
 	$(CC) -lz -D DEBUG -o modify-debug src/LibrePIA.c src/modify.c
 	$(CC) -lz -o decompress src/LibrePIA.c src/unzip.c
 	$(CC) -lz -D DEBUG -o decompress-debug src/LibrePIA.c src/unzip.c
-	$(CC) -lz -o new_ctb src/LibrePIA.c src/new_ctb.c
-	$(CC) -lz -D DEBUG -o new_ctb-debug src/LibrePIA.c src/new_ctb.c
-	$(CC) -lz -o new_stb src/LibrePIA.c src/new_stb.c
-	$(CC) -lz -D DEBUG -o new_stb-debug src/LibrePIA.c src/new_stb.c
+	$(CC) -lz -o ctb_new src/LibrePIA.c src/new_ctb.c
+	$(CC) -lz -D DEBUG -o ctb_new-debug src/LibrePIA.c src/new_ctb.c
+	$(CC) -lz -o stb_new src/LibrePIA.c src/new_stb.c
+	$(CC) -lz -D DEBUG -o stb_new-debug src/LibrePIA.c src/new_stb.c
+	$(CC) -lz -o stb_add_style src/LibrePIA.c src/add_style.c
+	$(CC) -lz -D DEBUG -o stb_add_style-debug src/LibrePIA.c src/add_style.c
+	$(CC) -lz -o stb_remove_style src/LibrePIA.c src/remove_style.c
+	$(CC) -lz -D DEBUG -o stb_remove_style-debug src/LibrePIA.c src/remove_style.c
 
 .PHONY: clean
 clean:
@@ -16,7 +20,11 @@ clean:
 	rm -f modify-debug
 	rm -f decompress
 	rm -f decompress-debug
-	rm -f new_ctb
-	rm -f new_ctb-debug
-	rm -f new_stb
-	rm -f new_stb-debug
+	rm -f ctb_new
+	rm -f ctb_new-debug
+	rm -f stb_new
+	rm -f stb_new-debug
+	rm -f stb_add_style
+	rm -f stb_add_style-debug
+	rm -f stb_remove_style
+	rm -f stb_remove_style-debug
---
+++ src/add_style.c
@@ -0,0 +1,55 @@
+/*
+ * LibrePIA is an attempt to create a LIBrary to Read and Edit PIA plot files (.pc3, .pmp, .stb, .ctb).
+ *
+ * Copyright (C) 2020 Paolo Caroni <kenren89@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* WARNING, the LibrePIA project at the moment is only a proof of concept*/
+
+/* include dependancy*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <zlib.h>
+/* include header files*/
+#include "LibrePIA.h"
+#include "plot_style_table.h"
+
+ /* proof of concept for add a plot style on an stb*/
+ int main(int argc, char **argv)
+ {
+    read_header(argv[1]);
+    decompress_data(argv[1],argv[2]);
+    /* verify subclass type*/
+    if (header[19]=='S' && header[20]=='T' && header[21]=='B')
+    {
+       /* parse stb*/
+       plot_style_parser(argv[2]);
+       fprintf(stderr, "Sorry, actually can be readed, writed or edited max %d styles, your file have %d styles.\n\n", MAX_STYLE, total_style_number);
+       add_plot_style_stb();
+       fprintf(stderr,"actually your file will have %d styles.", total_style_number);
+       /* rewrite txt for stb*/
+       plot_style_writer(argv[2]);
+    }
+
+    /* if in the header there is something wrong*/
+    else
+    {
+       fprintf(stderr, "WARNING:This isn't a STB\n\n");
+    }
+
+    /* compress data to other*/
+    write_PIA(argv[2], argv[3]);
+ }
+
+
